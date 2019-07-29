7dd14f611d2f68b4a634b05dc6299bcdcd0a64b0
Renee Sims <info@reneesoul.com>
2019-07-29 22:59:28 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,51 @@
 
+# def my_collect(collect)
+#   i = 0
+#   languages = []
+#   first_names = []
+  
+# # hash = Hash.new
+# # %w(cat dog wombat).each_with_index { |item, index|
+# #   hash[item] = index
+# # }
+# # hash   #=> {"cat"=>0, "dog"=>1, "wombat"=>2}
 
+# collect.each_with_index {languages.each_with_index << yield(collect[i]).upcase}
+# collect.each_with_index {first_names.each_with_index << yield(collect[i]).split(" ").first}
+#     i += 1
+#   # end
+  
+#   first_names
+#   return languages
+
+# end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def my_collect(collection)
+  i = 0
+  new_array = []
+
+  while i < collection.length 
+    new_array << yield(collection[i])
+        i += 1
+  end
+  
+  new_array
+
+end
